c10f224f6c0778cc229a440be302bb858917a92a
Brybug07 <146829081+Brybug07@users.noreply.github.com>
2024-10-04 15:47:29 +0100

    v16

--- Assets/Scripts/Room_Transition.cs
+++ Assets/Scripts/Room_Transition.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransition : MonoBehaviour
+public class Room_Transition : MonoBehaviour
 {
     // Set the name of the scene you want to transition to in the Inspector
     public string Scene2;
 
+   /*
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("trigger true");
+        
         // Check if the object entering the trigger is the player
         if (other.CompareTag("Player"))
         {
+            Debug.Log("tag true");
+            
             // Load the specified scene
             SceneManager.LoadScene(Scene2);
         }
     }
+   */
+   
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("collision true");
+        
+        // Check if the object entering the trigger is the player
+        if (collision.collider.CompareTag("Player"))
+        {
+            //Debug.Log("tag true");
+
+            // Load the specified scene
+            SceneManager.LoadScene(Scene2);
+        }
+    }
+
+   
 }
