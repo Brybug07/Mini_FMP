8e2577fc0992909c8c14573f479413f958850f56
Brybug07 <146829081+Brybug07@users.noreply.github.com>
2024-10-08 13:54:26 +0100

    v18

--- Assets/Scripts/Scene_Change.cs
+++ Assets/Scripts/Scene_Change.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI; // Required for UI components
 
 public class SceneChanger : MonoBehaviour
 {
-    public string Scene1; // The name of the scene to load
+    public string Scene; // The name of the scene to load
 
     // This method can be linked to a button click
     public void ChangeScene()
     {
-        SceneManager.LoadScene(Scene1);
+        SceneManager.LoadScene(Scene);
     }
 }
